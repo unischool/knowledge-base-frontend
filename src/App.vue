2e5a5cd107be56fea5419af6d48c5be3a5c317ab
Bestian Tang <bestian@gmail.com>
2024-12-13 07:50:14 +0800

    RWD and fix icons

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,24 @@ header
   nav.ui.fixed.top.menu
     button.no-border.ui.item(@click="toggleSidebar")
       i.icon.bars
-    RouterLink.item(to="/") 首頁-文件搜尋
+    RouterLink.item(to="/")
+      i.home.icon
+      | 首頁-文件搜尋
     //- RouterLink.item(to="/dragotesting") Drago測試
-    RouterLink.item(to="/ai") AI機器人-創源工具RAG
-    RouterLink.item(to="/ai-rag") AI機器人-學校課程RAG
-    RouterLink.item(to="/fulltextsearch") 全文檢索-創源工具
-    RouterLink.item(to="/unifulltextsearch") 全文檢索-學校課程
-    RouterLink.item(to="/faq") FAQ
-    RouterLink.item(to="/about") ABOUT
-    RouterLink.item(to="/adminpanel") 創源工具後台管理
-    RouterLink.item(to="/uniadminpanel") 學校課程後台管理
-    RouterLink.item(to="/upload") 上傳檔案
+    RouterLink.item(to="/ai")
+      i.chess.king.icon
+      | AI機器人
+      span.fat-only -創源工具RAG
+    RouterLink.item.fat-only(to="/ai-rag")
+      i.chess.queen.icon
+      | AI機器人-學校課程RAG
+    RouterLink.item.fat-only(to="/fulltextsearch") 全文檢索-創源工具
+    RouterLink.item.fat-only(to="/unifulltextsearch") 全文檢索-學校課程
+    RouterLink.item.fat-only(to="/faq") FAQ
+    RouterLink.item.fat-only(to="/about") ABOUT
+    RouterLink.item.fat-only(to="/adminpanel") 創源工具後台管理
+    RouterLink.item.fat-only(to="/uniadminpanel") 學校課程後台管理
+    RouterLink.item.fat-only(to="/upload") 上傳檔案
 .small-spacer
 .ui.sidebar.vertical.menu#side-menu(:class="{'hidden': !sidebarVisible}")
   RouterLink.item(to='/', exact='', name="home")
@@ -23,16 +30,16 @@ header
   //-   i.dragTesting.icon
   //-   | Drago測試
   RouterLink.item(to='/ai', name="ai")
-    i.ai.icon
+    i.chess.king.icon
     | AI機器人-創源工具RAG
   RouterLink.item(to='/ai-rag', name="ai-rag")
-    i.ai.icon
+    i.chess.queen.icon
     | AI機器人-學校課程RAG
   RouterLink.item(to='/fulltextsearch', name="fulltextsearch")
-    i.fulltextsearch.icon
+    i.search.icon
     | 全文檢索-創源工具
   RouterLink.item(to='/unifulltextsearch', name="unifulltextsearch")
-    i.unifulltextsearch.icon
+    i.search.icon
     | 全文檢索-學校課程
   RouterLink.item(to='/faq', name="faq")
     i.help.icon
